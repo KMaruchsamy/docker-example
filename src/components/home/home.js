f9411fb5e227b5f3e3d83acfe9f93526ac2c1f0a
Tony Xavier <tonyxavier@live.com>
2015-11-03 08:31:16 -0500

    7884,7885,7886,7551 - Frontend function

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,5 @@
 import {Component, View} from 'angular2/angular2';
-import {Router,Location} from 'angular2/router';
+import {Router, Location} from 'angular2/router';
 import {PageHeader} from '../shared/page-header';
 import {DashboardHeader} from './dashboard-header';
 import {DashboardPod1} from './dashboard-pod1';
@@ -7,37 +7,49 @@ import {DashboardPod2} from './dashboard-pod2';
 import {DashboardPod3} from './dashboard-pod3';
 import {DashboardPod4} from './dashboard-pod4';
 import {Auth} from '../../services/auth';
+import {Common} from '../../services/common';
 
 @Component({
-    selector: 'home',
-    viewBindings:[Auth]
+  selector: 'home',
+  viewBindings: [Auth, Common]
 })
 @View({
   templateUrl: '../../templates/home/home.html',
-  directives:[PageHeader,DashboardHeader,DashboardPod1,DashboardPod2,DashboardPod3,DashboardPod4]
+  directives: [PageHeader, DashboardHeader, DashboardPod1, DashboardPod2, DashboardPod3, DashboardPod4]
 })
 export class Home {
-  constructor(router: Router, auth:Auth,location:Location) {
-      this.router = router;
-      this.auth = auth;
-      this.location=location;
-      this.redirectToPage();
-      this.initialize();
+  constructor(router: Router, auth: Auth, location: Location, common: Common) {
+    this.common = common;
+    this.router = router;
+    this.auth = auth;
+    this.location = location;
+    this.redirectToPage();
+    this.initialize();
   }
-  redirectToPage(){
-      if(this.location.path().search("first")>0){
-          if((this.auth.istemppassword=="true"?true:false) && this.auth.isAuth())
-              this.router.parent.navigateByUrl('/login');
-      }
-      else if (!this.auth.isAuth())
-          this.router.parent.navigateByUrl('/login');
+  redirectToPage() {
+    if (this.location.path().search("first") > 0) {
+      if ((this.auth.istemppassword == "true" ? true : false) && this.auth.isAuth())
+        this.router.parent.navigateByUrl('/login');
+    }
+    else if (!this.auth.isAuth())
+      this.router.parent.navigateByUrl('/login');
   }
-  initialize(){
+
+  initialize() {
     $('title').html('Faculty Home Page');
   }
 
   redirectToLogin(event) {
-    event.preventDefault(); 
-    this.router.parent.navigateByUrl('/login');    
+    event.preventDefault();
+    this.router.parent.navigateByUrl('/login');
+  }
+
+  redirectToStudentSite(url, form, hdEmail, hdToken, hdURL) {
+    var serverURL = this.common.nursingITServer + this.common.config.links.nursingit.landingpage;
+    hdEmail.value = this.auth.useremail;
+    hdToken.value = this.auth.authheader;
+    hdURL.value = url;
+    $(form).attr('ACTION', serverURL).submit();
+    return false
   }
 }
--- src/services/common.js
+++ src/services/common.js
@@ -1,7 +1,11 @@
 ﻿export class Common {
     constructor() {
         this.sStorage = this.getStorage();
-     }
+        this.apiServer = '';
+        this.nursingITServer = '';
+        this.errorMessages = this.fetchErrorMessages();
+        this.config = this.fetchConfig();
+    }
 
     getErrorMessages() {
         return fetch('../config/error-messages.json');
@@ -29,5 +33,51 @@
         else
             return window.sessionStorage;
     }
+
+    fetchErrorMessages() {
+       let self = this;
+       fetch('../config/error-messages.json').then(function (response) {
+            return response.json()
+        }).then(function (json) {
+            self.errorMessages = json
+        }).catch(function (ex) {
+            console.log('parsing failed', ex)
+        });
+    }
+
+    fetchConfig() {
+        let self = this;
+        fetch('../config/config.json').then(function (response) {
+            return response.json()
+        }).then(function (json) {
+            self.config = json
+            self.getApiServer();
+            self.getNursingITServer();
+        }).catch(function (ex) {
+            console.log('parsing failed', ex)
+        });
+    }
+
+
+    getApiServer() {
+        let configJSON = this.config;
+        if (location.hostname.indexOf('localhost') > -1)
+            this.apiServer = configJSON.links.api.local.server;
+        if (location.hostname.indexOf('dev') > -1)
+            this.apiServer = configJSON.links.api.dev.server;
+        if (location.hostname.indexOf('qa') > -1)
+            this.apiServer = configJSON.links.api.qa.server;
+    }
+
+    getNursingITServer() {
+        let configJSON = this.config;
+        if (location.hostname.indexOf('localhost') > -1)
+            this.nursingITServer = configJSON.links.nursingit.local.server;
+        if (location.hostname.indexOf('dev') > -1)
+            this.nursingITServer = configJSON.links.nursingit.dev.server;
+        if (location.hostname.indexOf('qa') > -1)
+            this.nursingITServer = configJSON.links.nursingit.qa.server;
+    }
+
 
 }
