f3007f6d2cd97a3e469a748e3247d882408d9da4
swatikumari <swati.kumari@kaplan.com>
2015-12-30 13:09:10 +0530

    Reading IP address in case SSL not working & redirecting to appropriate url

--- src/app/services/common.ts
+++ src/app/services/common.ts
@@ -33,6 +33,10 @@ export class Common {
             return links.api.dev.server;
         if (location.hostname.indexOf('qa') > -1)
             return links.api.qa.server;
+        if (location.href.indexOf('81') > -1) // Incase if SSL not working then to read from IP address
+            return links.api.qa.server;
+        if (location.href.indexOf('80') > -1) // Incase if SSL not working then to read from IP address
+            return links.api.dev.server;
     }
 
     getNursingITServer() {
@@ -42,5 +46,9 @@ export class Common {
             return links.nursingit.dev.server;
         if (location.hostname.indexOf('qa') > -1)
             return links.nursingit.qa.server;
+        if (location.href.indexOf('81') > -1) // Incase if SSL not working then to read from IP address
+            return links.nursingit.qa.server;
+        if (location.href.indexOf('80') > -1) // Incase if SSL not working then to read from IP address
+            return links.nursingit.dev.server;
     }
 }
