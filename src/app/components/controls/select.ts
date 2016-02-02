980fd0240a1416c1c1c3457eebcf02fcba8ee389
Tony Xavier <tonyxavier@live.com>
2016-02-02 15:41:23 -0500

    Bug fixes & Review tests

--- src/app/components/controls/select.ts
+++ src/app/components/controls/select.ts
@@ -1,20 +1,34 @@
-import {Component,Input} from 'angular2/core';
+import {Component, Input, Output, EventEmitter} from 'angular2/core';
 import {NgFor} from 'angular2/common';
 @Component({
     selector: "ng2select",
     template: `
-     <select #ng2select (change)="loadTests(subjectDropDown.value)"
-            class="selectpicker" data-width="400" data-size="6" title='Choose a test by subject'>
+<label class="label-bold label-larger" for="{{selectId}}">{{selectText}}</label>
+     <select #ng2select (change)="onChange(ng2select,$event);"
+            class="selectpicker" [attr.data-width]="dataWidth" [attr.data-size]="dataSize" [attr.title]='title'>
       <option *ngFor="#option of options; #i = index" [selected]='option.value===selectedValue' [value]='option.value'>{{option.text}}</option>
      </select>
     `,
-    inputs:['options','selectedValue']
+    inputs: [
+        'options',
+        'selectedValue',
+        'selectId',
+        'selectText',
+        'dataWidth',
+        'dataSize',
+        'title'
+    ]
 })
-export class Ng2Select{
+export class Ng2Select {
     @Input() options: Object[];
     @Input() selectedValue: string;
-    
+    @Output('onSelectChange') onSelectChange = new EventEmitter();
     constructor() {
-        
+
     }
+    
+    onChange(ng2select, $event) {
+
+    }
+
 }
---
+++ src/app/components/tests/review-test.ts
@@ -0,0 +1,72 @@
+import {Component, OnInit} from 'angular2/core';
+import {Router, RouterLink} from 'angular2/router';
+import {NgIf} from 'angular2/common';
+import {TestService} from '../../services/test.service';
+import {Auth} from '../../services/auth';
+import {links} from '../../constants/config';
+import {PageHeader} from '../shared/page-header';
+import {PageFooter} from '../shared/page-footer';
+import {TestHeader} from './test-header';
+import * as _ from '../../lib/index';
+import {ParseDatePipe} from '../../pipes/parseDate.pipe';
+import {TestScheduleModel} from '../../models/testSchedule.model';
+
+@Component({
+    selector: "review-test",
+    templateUrl: "../../templates/tests/review-test.html",
+    providers: [TestService, Auth, TestScheduleModel],
+    directives: [PageHeader, TestHeader, PageFooter, NgIf, RouterLink],
+    pipes: [ParseDatePipe]
+})
+
+export class ReviewTest implements OnInit {
+    testScheduleWindow: string = '';
+    isScheduleDatesSame: boolean = true;
+    testScheduleDates: string = '';
+    testScheduleTimes: string = '';
+    constructor(public testScheduleModel: TestScheduleModel,
+        public testService: TestService, public auth: Auth, public router: Router) {
+        this.initialize();
+    }
+
+    ngOnInit() {
+
+    }
+    initialize() {
+        let savedSchedule = this.testService.getTestSchedule();
+        if (savedSchedule) {
+            this.testScheduleModel = savedSchedule;
+            let testScheduleWindowDuration = moment.duration(moment(this.testScheduleModel.scheduleEndTime).diff(this.testScheduleModel.scheduleStartTime));
+
+            this.testScheduleWindow = moment(this.testScheduleModel.scheduleEndTime).diff(this.testScheduleModel.scheduleStartTime, 'hours') + ' hour';
+            if (testScheduleWindowDuration.minutes() > 0)
+                this.testScheduleWindow += ' ' + testScheduleWindowDuration.minutes() + ' minute';
+            this.testScheduleWindow += ' window';
+
+
+
+
+            if (this.testScheduleModel.scheduleStartTime && moment(this.testScheduleModel.scheduleStartTime).isValid()) {
+                this.testScheduleDates = moment(this.testScheduleModel.scheduleStartTime).format('dddd') + ', ' + moment(this.testScheduleModel.scheduleStartTime).format('L');
+            }
+            if (this.testScheduleModel.scheduleEndTime && moment(this.testScheduleModel.scheduleEndTime).isValid() && moment(this.testScheduleModel.scheduleEndTime).isAfter(this.testScheduleModel.scheduleStartTime, 'days')) {
+                this.testScheduleDates = 'From ' + this.testScheduleDates + ' to ' + moment(this.testScheduleModel.scheduleEndTime).format('dddd') + ', ' + moment(this.testScheduleModel.scheduleEndTime).format('L');
+            }
+
+            if (this.testScheduleModel.scheduleStartTime && moment(this.testScheduleModel.scheduleStartTime).isValid()) {
+                this.testScheduleTimes = moment(this.testScheduleModel.scheduleStartTime).format('LT');
+            }
+            if (this.testScheduleModel.scheduleEndTime && moment(this.testScheduleModel.scheduleEndTime).isValid()) {
+                this.testScheduleTimes = 'From ' + this.testScheduleTimes + ' to ' + moment(this.testScheduleModel.scheduleEndTime).format('LT');
+            }
+
+            alert(this.testScheduleDates);
+            alert(this.testScheduleTimes);
+
+        }
+
+        if (this.testScheduleModel.currentStep < 3)
+            this.testScheduleModel.currentStep = 3;
+        this.testScheduleModel.activeStep = 3;
+    }
+}
