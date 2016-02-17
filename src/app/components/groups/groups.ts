af99b38b431b794905fcb47f98fa5f178ad24154
Tony Xavier <tonyxavier@live.com>
2016-02-17 12:34:56 -0500

    New pages

---
+++ src/app/components/groups/groups.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from 'angular2/core';
+import {Router, RouterLink} from 'angular2/router';
+import {Auth} from '../../services/auth';
+import {Common} from '../../services/common';
+import * as _ from '../../lib/index';
+import {PageHeader} from '../shared/page-header';
+import {PageFooter} from '../shared/page-footer';
+
+@Component({
+    selector: 'groups',
+    providers: [Auth],
+    templateUrl: '../../templates/groups/groups.html',
+    directives: [RouterLink, PageHeader, PageFooter]
+})
+
+export class Groups implements OnInit {
+    institutionName: string;
+    constructor(public auth: Auth, public router: Router) {
+
+    }
+
+    ngOnInit(): void {
+        if (!this.auth.isAuth())
+            this.router.navigateByUrl('/');
+        else
+            this.institutionName = this.getLatestInstitution();
+    }
+
+    getLatestInstitution(): string {
+        if (this.auth.institutions != null && this.auth.institutions != 'undefined') {
+            let latestInstitution = _.first(_.sortByOrder(JSON.parse(this.auth.institutions), 'InstitutionId', 'desc'))
+            if (latestInstitution)
+                return latestInstitution.InstitutionName;
+        }
+        return '';
+    }
+}
