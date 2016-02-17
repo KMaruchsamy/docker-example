8b02b4ed5703d2866e094ebfb0243ba94368a958
Tony Xavier <tonyxavier@live.com>
2016-02-17 17:15:05 -0500

    Reports activate one link

--- src/app/components/reports/reports.ts
+++ src/app/components/reports/reports.ts
@@ -5,16 +5,48 @@ import {Common} from '../../services/common';
 import * as _ from '../../lib/index';
 import {PageHeader} from '../shared/page-header';
 import {PageFooter} from '../shared/page-footer';
+import {HomeService} from '../../services/home-service';
+import {links} from '../../constants/config';
 
 @Component({
     selector: 'reports',
-    providers: [Auth],
+    providers: [Auth, Common],
     templateUrl: '../../templates/reports/reports.html',
     directives: [RouterLink, PageHeader, PageFooter]
 })
 
 export class Reports {
-    constructor(public auth: Auth, public router: Router) {
-
+    institutionRN: number;
+    institutionPN: number;
+    page: string;
+    form: any;
+    hdInstitution: any;
+    hdToken: any;
+    hdURL: any;
+    hdpage: any;
+    apiServer: string;
+    nursingITServer: string;
+    constructor(public auth: Auth, public router: Router, public common: Common) {
+        this.apiServer = this.common.getApiServer();
+        this.nursingITServer = this.common.getNursingITServer();
     }
+
+    prepareRedirectToReports(page, form, hdToken, hdpage): boolean {
+        this.page = page;
+        this.form = form;
+        this.hdToken = hdToken;
+        this.hdpage = hdpage;
+        this.redirectToReports();
+        return false;
+    }
+
+    redirectToReports(): void {
+        var serverURL = this.nursingITServer + links.nursingit.ReportingLandingPage;
+        this.hdToken.value = this.auth.token;
+        this.hdpage.value = this.page;
+        $(this.form).attr('ACTION', serverURL).submit();
+    }
+
 }
+
+
