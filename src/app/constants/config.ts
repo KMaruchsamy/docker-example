0ff4fe8a36a8fa4641988fc470bfec3b52f50b9e
swatikumari <swati.kumari@kaplan.com>
2016-05-24 12:23:21 +0530

    After resolving conflict showing file changes

--- src/app/constants/config.ts
+++ src/app/constants/config.ts
@@ -28,7 +28,7 @@ export const links = {
             profilesapi: "profile",
             test: {
                 subjects: "scheduling/institutions/§institutionid/subjects?testType=§testtype",
-                tests: "scheduling/institutions/§institutionid/tests?subject=§subject&testType=§testtype",
+                tests: "scheduling/institutions/§institutionid/tests?subject=§subject&testType=§testtype&searchString=§searchString", //Added Searchstring parameter.
                 openintegratedtests: "scheduling/OpenIntegratedTests",
                 cohorts: "scheduling/institutions/§institutionid/activeCohorts?windowStart=§windowstart&windowEnd=§windowend",
                 cohortstudents: "scheduling/cohorts/§cohortid/students?testId=§testid",
