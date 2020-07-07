8ba1e69a3e49c0289f53b8994d10cbd7093b16e3
swati <swati@K26s-MacBook-Pro.local>
2020-07-07 20:38:28 +0530

    fix related to li-3707 and li-3724

--- src/app/components/shared/page-header.component.ts
+++ src/app/components/shared/page-header.component.ts
@@ -1,20 +1,15 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-// import {AuthService} from '../../services/auth';
-// import {DropdownMenu} from '../controls/dropdown-menu';
 import { AuthService } from './../../services/auth.service';
-// import { HeaderComponent } from "../header/header.component";
 
 
 @Component({
     selector: 'page-header',
-    // providers: [AuthService],
     templateUrl: './page-header.component.html',
     styleUrls: ['./page-header.component.scss']
-    // directives: [, DropdownMenuComponent, NgClass, RouterLinkActive]
-})
+ })
 
-export class PageHeaderComponent implements OnChanges{
+export class PageHeaderComponent implements OnInit{
     @Input() showCover: boolean;
     @Input() ariaDisabled: boolean;
     @Input() hideDropdown: boolean;
@@ -22,12 +17,9 @@ export class PageHeaderComponent implements OnChanges{
     username: string;
 
   constructor(public router: Router, public auth: AuthService) {
-    this.updateHeaderBasedOnTemplate();
-    // this.showCover = true;
-    // this.hideDropdown = true;
-  }
-  ngOnChanges() {
-    this.updateHeaderBasedOnTemplate();
+ }
+  ngOnInit() {
+     this.updateHeaderBasedOnTemplate();
   }
   updateHeaderBasedOnTemplate() {
     this.username = this.auth.firstname + ' ' + this.auth.lastname;
