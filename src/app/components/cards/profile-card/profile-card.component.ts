ffdb29a99a4b21e20befd4a898b71b9f3020b814
Aparna Singh <aparna.singh@kaplan.com>
2020-07-17 11:40:28 +0530

    Li-3717 profile card

--- src/app/components/cards/profile-card/profile-card.component.ts
+++ src/app/components/cards/profile-card/profile-card.component.ts
@@ -1,10 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { MatSelect } from "@angular/material/select";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-profile-card',
-  templateUrl: './profile-card.component.html',
-  styleUrls: ['./profile-card.component.scss']
+  selector: "app-profile-card",
+  templateUrl: "./profile-card.component.html",
+  styleUrls: ["./profile-card.component.scss"]
 })
-export class ProfileCardComponent{
+export class ProfileCardComponent implements OnInit {
   @Input() card;
+  flag: boolean;
+  constructor(public router: Router) {}
+  ngOnInit() {
+    this.flag = true;
+  }
+  onLinkChange(event) {
+    this.flag = false;
+  }
 }
