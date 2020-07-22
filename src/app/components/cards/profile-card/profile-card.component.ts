721840c607e973dfec40b61bd6574614b048cc54
Aparna Singh <aparna.singh@kaplan.com>
2020-07-22 13:55:13 +0530

    3717 bug-fix

--- src/app/components/cards/profile-card/profile-card.component.ts
+++ src/app/components/cards/profile-card/profile-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { MatSelect } from "@angular/material/select";
 import { Router } from "@angular/router";
+import { links } from "../../../constants/config";
 
 @Component({
   selector: "app-profile-card",
@@ -10,12 +11,14 @@ import { Router } from "@angular/router";
 export class ProfileCardComponent implements OnInit {
   @Input() card;
   flag: boolean;
+  defaultProfileImage;
   constructor(public router: Router) {}
   ngOnInit() {
+    this.defaultProfileImage = links.profile.nurseconsultant.defaultimage;
     this.flag = true;
   }
-  routeTo(event){
+  routeTo(event) {
     this.flag = false;
-    window.open(event,'_blank');
+    window.open(event, "_blank");
   }
 }
