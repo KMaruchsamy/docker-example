947fc4b033a547ed7621c3a130ee8323ebedcb65
Tony Xavier <tonyxavier@live.com>
2020-07-22 14:09:37 -0400

    Appcues (#22)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,32 @@
 /// <reference path="../typings.d.ts" />
 
-import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Component } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  template:`<router-outlet></router-outlet>`,
+  selector: "app-root",
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
-  constructor(public titleService: Title) {
-       
-    }
-    public setTitle(newTitle: string) {
-        this.titleService.setTitle(newTitle);
-    }
+  constructor(public titleService: Title, private router: Router) {}
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 
-    ngOnInit(): void {
-        this.setTitle('Kaplan Nursing');
-    }
+  ngOnInit(): void {
+    this.setTitle("Kaplan Nursing");
+    (window as any).Appcues.anonymous();
+    this.reloadAppcues();
+    (window as any).Appcues.track("Testing from faculty ui");
+  }
+
+  reloadAppcues() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // tslint:disable-next-line: no-unused-expression
+        (window as any).Appcues && (window as any).Appcues.page();
+      }
+    });
+  }
 }
