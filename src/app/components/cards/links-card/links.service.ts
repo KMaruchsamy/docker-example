6204887c000a2bc8c078b3a2f31ea87062d64d6e
swati <swati@K26s-MacBook-Pro.local>
2020-08-07 08:42:28 +0530

    added missing &

--- src/app/components/cards/links-card/links.service.ts
+++ src/app/components/cards/links-card/links.service.ts
@@ -64,7 +64,7 @@ export class LinksService implements OnDestroy {
 
     let url: string;
     if(s.value ==='ApolloStudentReportCard')
-      url = `${this.nitServer}${links.nursingit.apolloLaunchPage}${"adminId="}${this.auth.userid}`;
+      url = `${this.nitServer}${links.nursingit.apolloLaunchPage}${"&adminId="}${this.auth.userid}`;
     else
       url = `${this.nitServer}${links.nursingit.ReportingLandingPage}`;
 
