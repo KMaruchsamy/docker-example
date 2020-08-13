ade6d3aad1f9f6a8ec6cccc00dcebf10aed2cdbc
Tony <tonyxavier@live.com>
2020-08-13 11:01:15 -0400

    GA changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,33 @@
 import { Component } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { NavigationEnd, Router } from "@angular/router";
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 @Component({
-  selector: "app-root",
-  template: `<router-outlet></router-outlet>`,
+  selector: 'app-root',
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
-  constructor(public titleService: Title, private router: Router) {}
+  constructor(
+    public titleService: Title,
+    private router: Router,
+    angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics
+  ) {
+    angulartics2GoogleAnalytics.startTracking();
+  }
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
 
   ngOnInit(): void {
-    this.setTitle("Kaplan Nursing");
+    this.setTitle('Kaplan Nursing');
     (window as any).Appcues.anonymous();
     this.reloadAppcues();
-    (window as any).Appcues.track("Testing from faculty ui");
+    (window as any).Appcues.track('Testing from faculty ui');
   }
 
   reloadAppcues() {
-    this.router.events.subscribe((event) => {
+    this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         // tslint:disable-next-line: no-unused-expression
         (window as any).Appcues && (window as any).Appcues.page();
