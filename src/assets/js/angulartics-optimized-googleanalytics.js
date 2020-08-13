ff3aaa5974b0b2bd66123bba9a5d03ae374e8c5a
Tony <tonyxavier@live.com>
2020-08-13 01:28:31 -0400

    Change GA

--- src/assets/js/angulartics-optimized-googleanalytics.js
+++ src/assets/js/angulartics-optimized-googleanalytics.js
@@ -1,9 +1,9 @@
 
 var _gaq = _gaq || [];
-if (window.location.hostname === 'nit.kaplan.com')
-    _gaq.push(['_setAccount', 'UA-72468900-1']);
-else if (window.location.hostname === 'qa-nit.kaplan.com')
-    _gaq.push(['_setAccount', 'UA-72468900-2']);
+if (window.location.hostname === 'nit.kaptest.com')
+  _gaq.push(['_setAccount', 'UA-72468900-1']);
+else if (window.location.hostname === 'nit.qa.kaptest.com')
+       _gaq.push(['_setAccount', 'UA-72468900-2']);
 _gaq.push(['_trackPageview']);
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
