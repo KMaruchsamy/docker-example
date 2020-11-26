23d07f8ac860be44e21f01158d4c617e61d08f18
swati <swati@K26s-MacBook-Pro.local>
2020-11-26 17:24:52 +0530

    testing checkbox Issue

--- src/app/components/rosters/rosters-change-update-form.component.ts
+++ src/app/components/rosters/rosters-change-update-form.component.ts
@@ -113,6 +113,7 @@ export class RostersChangeUpdateFormComponent implements OnInit, OnDestroy {
                     null,
                     null,
                     { "width": "100px" },
+                    { "width": "100px" },
                     { "width": "100px" }
                 ],
                 "responsive": {
@@ -153,6 +154,16 @@ export class RostersChangeUpdateFormComponent implements OnInit, OnDestroy {
                                     $('input', $html).attr('id', _id + "-" + rowIdx.toString());  // change the element id for responsive mode
                                     var el = $('input', $html);
                                     self.onADAChange(el);
+                                    var _labelId = $($html[2].innerHTML).attr('for');
+                                    $('label', $html).attr('for', _labelId + "-" + rowIdx.toString());  // change the element id for responsive mode
+                                    _data = $html[0].innerHTML + $html[2].innerHTML;
+                                }
+
+                                else if (cell.column === 4) {
+                                    var _id = $('input', $html).attr('id');
+                                    $('input', $html).attr('id', _id + "-" + rowIdx.toString());  // change the element id for responsive mode
+                                    var el = $('input', $html);
+                                    self.onActiveChange(el);
                                     var _labelId = $($html[2].innerHTML).attr('for');
                                     $('label', $html).attr('for', _labelId + "-" + rowIdx.toString());  // change the element id for responsive mode
                                     _data = $html[0].innerHTML + $html[2].innerHTML;
@@ -202,6 +213,15 @@ export class RostersChangeUpdateFormComponent implements OnInit, OnDestroy {
                 if (_id.indexOf('-') > 0) {
                     let _studentid = parseInt(_id.split('-')[0]);
                     self.checkGrantUntimedTest(_studentid, e);
+                    self.onChangingUserSelection(e);
+                }
+            });
+
+            $('#markChangesTable tbody').on('change', '.js-Active', function (e) {
+                var _id = $(e.target).attr('id').split('_')[1];
+                if (_id.indexOf('-') > 0) {
+                    let _studentid = parseInt(_id.split('-')[0]);
+                    self.checkMakeActive(_studentid, e);
                     self.onChangingUserSelection(e);
                 }
             });
@@ -298,6 +318,27 @@ export class RostersChangeUpdateFormComponent implements OnInit, OnDestroy {
         }
     }
 
+    onActiveChange(el) {
+        if(el.length>0){
+            let _selectedStudent: ChangeUpdateRosterStudentsModel;
+            var _id = el.attr('id').split('_')[1];
+            let _studentid = parseInt(_id.split('-')[0]);
+            _.filter(this.rosterChangeUpdateStudents, (s) => {
+                if (s.studentId == _studentid) {
+                    _selectedStudent = s;
+                }
+            });
+            if (_selectedStudent.IsInactiveForRostering !== null && !_selectedStudent.IsInactiveForRostering)
+                el.attr('checked', 'checked');
+            else
+                el.removeAttr('checked');
+            if (_selectedStudent.isInactive !== null && _selectedStudent.isInactive || _selectedStudent.userExpiryDate)
+                el.attr('disabled', 'true');
+            else
+                el.removeAttr('disabled');
+        }
+    }
+
         loadRosterCohortStudents(cohort: RosterCohortsModel, cohortStudents: any) {
         let savedData: RosterChangesModel = JSON.parse(this.sStorage.getItem('rosterChanges'));
         let savedStudents: Array<ChangeUpdateRosterStudentsModel> = [];
@@ -377,7 +418,10 @@ export class RostersChangeUpdateFormComponent implements OnInit, OnDestroy {
 
         //On Selection of ADA checkbox selection... Start   
         var _ADAElement = $('#' + 'ADA_' + _id);
-        this.onADAChange(_ADAElement);  
+        this.onADAChange(_ADAElement); 
+        
+        var _ActiveElement = $('#' + 'makeActive_' + _id);
+        this.onActiveChange(_ActiveElement); 
     }
 
     showCohortPopup(firstName, lastName, studentId, e) {
@@ -445,6 +489,24 @@ export class RostersChangeUpdateFormComponent implements OnInit, OnDestroy {
         this.changeUpdateRosterStudentsEvent.emit(student);
     }
 
+    checkMakeActive(_studentId, e) {
+        e.preventDefault();
+        let target = e.target || e.srcElement || e.currentTarget;
+        let isChecked: boolean = target.checked;
+        let student: ChangeUpdateRosterStudentsModel;
+        _.filter(this.rosterChangeUpdateStudents, function (_student) {
+            if (_student.studentId === _studentId) {debugger
+                _student.IsInactiveForRostering= isChecked;
+                _student.updateType = RosterUpdateTypes.MoveToDifferentCohort; 
+                if (_student.moveToCohortId === null) {
+                    _student.isInactive = false;
+                }
+                student = _student;
+            }
+        });
+
+        this.changeUpdateRosterStudentsEvent.emit(student);
+    }
     changeCohortTo(_student: ChangeUpdateRosterStudentsModel) {
         this.showRequestChangePopup = false;
         if (_student !== undefined) {
