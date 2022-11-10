5ae9cb9040101859a087d727e3e101be16af811f
Elvis Fernandes <85608381+elvisferns@users.noreply.github.com>
2022-11-10 12:36:02 +0530

    Revert "New chat widget"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,18 @@
 /// <reference path="../typings.d.ts" />
 
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { NavigationEnd, Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { NavigationEnd, Router } from "@angular/router";
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
-import { ChatWidgetService } from './services/chat-widget.service';
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   constructor(
     public titleService: Title,
     private router: Router,
-    public chatWidgetService: ChatWidgetService,
     angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics
   ) {
     angulartics2GoogleAnalytics.startTracking();
@@ -24,8 +22,8 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.chatWidgetService.loadChatWidget();
     this.setTitle('Kaplan Nursing');
+    
     this.reloadAppcues();
     (window as any).Appcues.track('Testing from faculty ui');
   }
