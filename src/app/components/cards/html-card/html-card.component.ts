3051d89ac5e85de448d99a113775291b11d0b27e
Elvis Fernandes <85608381+elvisferns@users.noreply.github.com>
2023-03-29 19:28:50 +0530

    add help text for NBS (#110)
    solve bug - throws error on click of help card on home-dashboard

--- src/app/components/cards/html-card/html-card.component.ts
+++ src/app/components/cards/html-card/html-card.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 import { AnalyticsService } from './../../../services/analytics.service';
 
 @Component({
-  selector: "app-html-card",
-  templateUrl: "./html-card.component.html",
-  styleUrls: ["./html-card.component.scss"],
+  selector: 'app-html-card',
+  templateUrl: './html-card.component.html',
+  styleUrls: ['./html-card.component.scss']
 })
 export class HtmlCardComponent {
   @Input() card;
   constructor(public ga: AnalyticsService) {}
 
   gaClick(e) {
-    let ga = this.card.ga;
-    let value = '';
-    if (e.target.getAttribute('href') && e.target.getAttribute('href').length > 0)
-      value= e.target.innerText;
-    this.ga.track(e.type,{category: ga.category, label: value});
+    if (this.card.ga) {
+      const ga = this.card.ga;
+      let value = '';
+      if (
+        e.target.getAttribute('href') &&
+        e.target.getAttribute('href').length > 0
+      ) {
+        value = e.target.innerText;
+      }
+      this.ga.track(e.type, { category: ga.category, label: value });
+    }
   }
 }
