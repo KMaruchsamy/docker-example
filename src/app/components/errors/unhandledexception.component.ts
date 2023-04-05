59a4a13ffac110ec4ca99d05e04af9ce2d0f7f3a
elvis <elvisfernandes.fernandes@kaplan.com>
2023-04-05 11:31:55 +0530

    modify unhandled error page for modify teesting session 401 API error

--- src/app/components/errors/unhandledexception.component.ts
+++ src/app/components/errors/unhandledexception.component.ts
@@ -1,34 +1,44 @@
-import {Component, OnInit} from '@angular/core';
-// import {} from '@angular/router';
-import {Title} from '@angular/platform-browser';
-import { PageHeaderComponent } from './../shared/page-header.component';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-    selector: 'unhandled-exception',
-    // directives: [, PageHeaderComponent],
-    template: `<page-header [hideDropdown]=true></page-header>
+  selector: 'unhandled-exception',
+  template: `<page-header [hideDropdown]="true"></page-header>
     <main role="main" class="app-main">
       <div class="section">
         <div class="container-narrow center">
           <i class="icon broken-heart-icon"></i>
-          <h2 class="margin-1em-top">We’re sorry. Something went wrong on our end.</h2>
-          <p class="text-larger">There was a problem loading this page.
-            We’re working to fix the problem as soon as possible.
-            We apologize for the inconvenience and thank you for your patience!
+          <h2 class="margin-1em-top">
+            We're sorry. Something went wrong on our end.
+          </h2>
+          <p class="text-larger">
+            There was a problem loading this page. We're working to fix the
+            problem as soon as possible. We apologize for the inconvenience and
+            thank you for your patience!
           </p>
+          <div class="section section-beige container-dull-yellow-border container-dull-yellow-top text-larger">
+            If you were in the process of <b>modifying a testing session</b>, please
+            use the back button in your browser to return to the process. If you
+            continue to have issues, please reach out to your customer
+            engagement team member.
+          </div>
           <h2 class="margin-2em-top">What can you do?</h2>
           <p class="text-larger">Try reloading the page in a few minutes.</p>
-          <p class="text-larger">Already signed in? Go to the <a [routerLink]="['/home']">home page</a>.</p>
-          <p class="text-larger">Need to sign in? Go to the <a [routerLink]="['/']">sign in page</a>.</p>
-       </div>
-    </div>
+          <p class="text-larger">
+            Already signed in? Go to the
+            <a [routerLink]="['/home']">home page</a>.
+          </p>
+          <p class="text-larger">
+            Need to sign in? Go to the <a [routerLink]="['/']">sign in page</a>.
+          </p>
+        </div>
+      </div>
     </main>`
 })
 export class UnhandledExceptionComponent implements OnInit {
-    constructor(public titleService: Title){
-    }
+  constructor(public titleService: Title) {}
 
-    ngOnInit(): void {
-        this.titleService.setTitle('Error – Kaplan Nursing');
-    } 
+  ngOnInit(): void {
+    this.titleService.setTitle('Error - Kaplan Nursing');
+  }
 }
